229672b56c34f529cadc47549d1beec1e5ec9744
Mike Murphy <codermurph@sleepymurph.com>
2015-10-24 21:21:25 +0200

    Rewrite: start server, but deadlocks

---
+++ assignment2/src/node2_http.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+import BaseHTTPServer
+import argparse
+import collections
+import httplib
+import signal
+import threading
+
+import node_core2 as ncore
+
+# Verbose output trace
+
+verbose = False
+
+def verbosep(msg):
+    """ Print string if verbose output is on """
+    if verbose:
+        if server_node_core:
+            print("%s: %s" % (server_node_core.descriptor.host_port, msg))
+        else:
+            print(strs)
+
+
+# HTTP Abstractions
+
+class HttpRequest:
+    """ Abstraction of an HTTP request to a node """
+    def __init__(self,
+            host=None, port=80,
+            destination=None,
+            method="POST", path="", body=""):
+
+        if host!=None:
+            self.host = host
+            self.port = port
+            self.destination = ncore.NodeDescriptor(host=host, port=port)
+
+        if isinstance(destination, ncore.NodeDescriptor):
+            self.destination = destination
+            self.host = destination.host
+            self.port = destination.port
+
+        self.method = method
+        self.path = path
+        self.body = body
+
+
+# Convert HTTP to and from Node Messages
+
+def parse_single_node_descriptor(s):
+    """ Parse a string as a single node descriptor host:port """
+    return ncore.NodeDescriptor(host_port = s.strip())
+
+
+def parse_request(hr):
+    """ Parse an HttpRequest to one of the node message types """
+
+    if hr.path=="join" and hr.method=="POST":
+        new_node = parse_single_node_descriptor(hr.body)
+        return ncore.Join(destination=hr.destination, new_node=new_node)
+
+def build_request(msg):
+    """ Build an HttpRequest for a node message """
+
+    if isinstance(msg, ncore.Join):
+        return HttpRequest(
+                destination = msg.destination,
+                method = "POST",
+                path = "join",
+                body = msg.new_node.host_port)
+
+
+# Actually Send Requests
+
+def send_message(msg):
+    """ Send a message to another node """
+    verbosep("sending %s" % str(msg))
+    hr = build_request(msg)
+    send_request(hr)
+
+
+def send_request(hr):
+    """ Actually send a request to a server via HTTP """
+
+    conn = httplib.HTTPConnection(hr.host, hr.port)
+    conn.request(hr.method, hr.path, hr.body)
+
+    # Must read response even if we don't do anything with it.
+    # If we don't, the server will get broken pipe errors.
+    #	1. Return without reading response
+    #	2. Server might not be finish sending response yet.
+    #	3. Library code here closes connection.
+    #	4. Server code tries to finish writing to closed pipe.
+    #	5. Broken pipe.
+    response = conn.getresponse()
+    data = response.read()
+
+    if response.status!=200:
+        raise
+
+
+# Actually Run as a Server
+
+server_node_core = None     # Core decision module for server
+
+class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    global server_node_core
+
+    def do_GET(self):
+        self.handle_request("GET")
+
+    def do_POST(self):
+        self.handle_request("POST")
+
+    def handle_request(self, method):
+        body = self.rfile.read()
+        msg = parse_request(HttpRequest(
+                destination = server_node_core.descriptor,
+                method = method,
+                path = self.path,
+                body = body))
+
+        verbosep("received %s" % msg)
+        action = server_node_core.handle_message(msg)
+
+        respond_ok()
+        for newmsg in action:
+            send_message(newmsg)
+
+    def respond_ok(self):
+        self.send_response(200)
+        self.end_heaers()
+        self.wfile.write("")
+
+
+
+# ----------------------------------------------------------
+# Basic HTTP server
+#
+class NodeServer(BaseHTTPServer.HTTPServer):
+
+    def server_bind(self):
+        BaseHTTPServer.HTTPServer.server_bind(self)
+        self.socket.settimeout(1)
+        self.run = True
+
+    def get_request(self):
+        while self.run == True:
+            try:
+                sock, addr = self.socket.accept()
+                sock.settimeout(None)
+                return (sock, addr)
+            except socket.timeout:
+                if not self.run:
+                    raise socket.error
+
+    def stop(self):
+        self.run = False
+
+    def serve(self):
+        while self.run == True:
+            self.handle_request()
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true",
+            help="debug trace")
+    parser.add_argument("host_port", metavar="host:port",
+            help="external hostname (or IP) and port for this server")
+    parser.add_argument("--join", metavar="host:port", default=None,
+            help="another server to join")
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    descriptor = ncore.NodeDescriptor(host_port=args.host_port)
+    join_descriptor = ncore.NodeDescriptor(host_port=args.join) if args.join else None
+    server_node_core = ncore.NodeCore(descriptor)
+
+    # Start the webserver which handles incomming requests
+    verbosep("Starting HTTP server: %s, %s" % (descriptor, descriptor.rank))
+    httpd = NodeServer((descriptor.host, descriptor.port), HttpRequestHandler)
+    server_thread = threading.Thread(target = httpd.serve)
+    server_thread.daemon = True
+    server_thread.start()
+
+    def handler(signum, frame):
+        verbosep("Stopping http server...")
+        httpd.stop()
+    signal.signal(signal.SIGINT, handler)
+
+    # Join network
+    if join_descriptor:
+        joinmsg = ncore.Join(destination=join_descriptor, new_node=descriptor)
+        send_message(joinmsg)
+
+    # Wait for server thread to exit
+    server_thread.join(100)
---
+++ assignment2/src/node2_http_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import unittest
+from pprint import pformat
+
+import node_core2 as ncore
+import node2_http as nhttp
+
+long_max_digit = 100000000000000000000000000000000000000L
+
+def node_ranked(rank):
+    desc = ncore.NodeDescriptor(host="127.0.0.1", port=rank)
+    desc.rank = rank * long_max_digit
+    return desc
+
+class TestParseBuild(unittest.TestCase):
+
+    def test_build_join(self):
+        d0 = ncore.NodeDescriptor(host_port="localhost:8000")
+        d1 = ncore.NodeDescriptor(host_port="localhost:8001")
+        msg = ncore.Join(destination=d0, new_node=d1)
+
+        req = nhttp.build_request(msg)
+
+        self.assertEqual(req.method, "POST")
+        self.assertEqual(req.path, "join")
+        self.assertEqual(req.body, "localhost:8001")
+
+    def test_parse_join(self):
+        receiver = ncore.NodeDescriptor(host_port="localhost:8000")
+        req = nhttp.HttpRequest(
+                destination = receiver,
+                method = "POST",
+                path = "join",
+                body = "localhost:8001\n")
+
+        msg = nhttp.parse_request(req)
+
+        self.assertEqual(isinstance(msg, ncore.Join), True)
+        self.assertEqual(msg.destination, receiver)
+        self.assertEqual(msg.new_node,
+                ncore.NodeDescriptor(host_port="localhost:8001"))
+
+
+if __name__ == '__main__':
+    unittest.main()
