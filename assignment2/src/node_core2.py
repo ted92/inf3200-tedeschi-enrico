7422b16603b6fcfa0e559c5810b98dd7ec95f0d8
Mike Murphy <mmu019@post.uit.no>
2015-10-24 16:17:07 +0200

    Start rewrite

---
+++ assignment2/src/node_core2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import hashlib
+import collections
+
+def node_hash(s):
+    """ Map input to the node key space """
+    md5 = hashlib.md5()
+    md5.update(s)
+    hexhash = md5.hexdigest()
+    numerichash = long(hexhash,16)
+    return numerichash
+
+
+class NodeDescriptor:
+    """ Essential node metadata for addressing and ranking a node """
+
+    def __init__(self, host=None, port=None, host_port=None):
+        if host!=None and port!=None:
+            (self.host, self.port) = (host, port)
+        elif host_port!=None:
+            (self.host, self.port) = host_port.split(":")
+        else:
+            raise RuntimeError( "Bad %s: host='%s', port='%s', host_port='%s'"
+                % (self.__class__.__name__, host, port, host_port) )
+
+        self.host_port = "%s:%s" % (self.host, self.port)
+        self.rank = node_hash(self.host_port)
+
+    def __repr__(self):
+        return ("<%s.%s %s,%s>"
+            % (self.__class__.__module__, self.__class__.__name__,
+                self.host_port, self.rank))
+
+    def __str__(self):
+        return "%s:%s" % (self.host, self.port)
+
+
+# Abstract Node Requests
+
+Join = collections.namedtuple("Join", ["new_node"])
+
+
+# Abstract Node Responses
+
+GenericOk = collections.namedtuple("GenericOk", [])
+
+ForwardRequest = collections.namedtuple("ForwardRequest",
+        ['destination','request'])
+
+JoinAccepted = collections.namedtuple("JoinAccepted", ['new_node', 'successor'])
+
+
+
+# Core Logic of Node
+
+class NodeCore:
+    def __init__(self, descriptor):
+        self.descriptor = descriptor        # This node's descriptor
+        self.successor = None               # Successor node's descriptor
+
+
+    def responsible_for_key(self, key):
+        """ Hashes the key and decides if the hash is in range to be handled by this node """
+        key_hash = node_hash(key)
+        return self.responsible_for_hash(key_hash)
+
+
+    def responsible_for_hash(self, key_hash):
+        """ Decides if the hash is in range to be handled by this node """
+
+        d = self.descriptor
+        s = self.successor
+
+        if (s == None):
+            # Single node
+            return True
+        elif (d.rank < s.rank):
+            # Normal
+            return d.rank <= key_hash and key_hash < s.rank
+        else:
+            # Wrap-around
+            return d.rank <= key_hash or key_hash < s.rank
+
+
+    def handle_request(self, ar):
+        """ Handle a request """
+
+        d = self.descriptor
+        s = self.successor
+
+        if isinstance(ar, Join):
+
+            n = ar.new_node
+
+            if self.responsible_for_hash(n.rank):
+
+                ns = s if s else d
+                self.successor = n
+                return JoinAccepted(new_node=n, successor=ns)
+
+            else:
+                return ForwardRequest(destination=s, request=ar)
+
+        if isinstance(ar, JoinAccepted):
+            self.successor = ar.successor
+            return GenericOk()
---
+++ assignment2/src/node_core2_test.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+import unittest
+from pprint import pformat
+
+import node_core2 as node
+
+class TestNodeHashing(unittest.TestCase):
+
+    def test_node_hash_to_number(self):
+        numeric_hash = node.node_hash("hello world!")
+        self.assertTrue(isinstance(numeric_hash, (int,long)),
+            "Expected hash to be numeric (int or long). Got %s: '%s'" %
+                (type(numeric_hash), numeric_hash) )
+
+    def test_node_hash_same_string_same_hash(self):
+        hash_a = node.node_hash("hello world!")
+        hash_b = node.node_hash("hello"+ " world!")
+        self.assertEqual(hash_a, hash_b)
+
+class TestNodeDescriptor(unittest.TestCase):
+
+    def test_construct(self):
+        nd = node.NodeDescriptor(host="127.0.0.1", port="8000")
+        self.assertEqual(nd.host, "127.0.0.1")
+        self.assertEqual(nd.port, "8000")
+
+    def test_parse(self):
+        nd = node.NodeDescriptor(host_port="127.0.0.1:8000")
+        self.assertEqual(nd.host, "127.0.0.1")
+        self.assertEqual(nd.port, "8000")
+
+    def test_bad_construct(self):
+        self.assertRaises(RuntimeError, node.NodeDescriptor)
+
+    def test_host_port(self):
+        nd = node.NodeDescriptor("127.0.0.1", "8000")
+        self.assertEqual(nd.host_port, "127.0.0.1:8000")
+
+    def test_rank(self):
+        nd = node.NodeDescriptor(host_port="127.0.0.1:8000")
+        self.assertEqual(nd.rank, node.node_hash("127.0.0.1:8000"))
+        self.assertEqual(nd.rank, 102808487155392830909659332955855849052L)
+
+
+long_max_digit = 100000000000000000000000000000000000000L
+
+def key_ranked(*args):
+    modulus = args[0]
+    TRIES_LIMIT=1000
+    for i in range(0, TRIES_LIMIT):
+        key = str(i)
+        if (node.node_hash(key) / long_max_digit == modulus):
+            return key
+    raise Exception("Could not generate an appropriate key")
+
+def node_ranked(rank):
+    desc = node.NodeDescriptor(host="127.0.0.1", port=rank)
+    desc.rank = rank * long_max_digit
+    return desc
+
+class TestNodeCore(unittest.TestCase):
+
+    def test_single_node_no_successor(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        self.assertEqual(n0.descriptor, d0)
+        self.assertEqual(n0.successor, None)
+
+    def test_single_always_responsible_for_key(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        k1 = key_ranked(1)
+        self.assertEqual(n0.responsible_for_key(k1), True)
+
+    def test_join_single(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        d1 = node_ranked(1);    n1 = node.NodeCore(d1)
+
+        # Join nodes 0 and 1
+        result = n0.handle_request(node.Join(d1))
+
+        # Original node should accept the request
+        self.assertEqual(isinstance(result, node.JoinAccepted), True)
+        self.assertEqual(result.successor, d0)
+
+        # Original node should accept new node as its successor
+        self.assertEqual(n0.successor, d1)
+
+        # New node should take existing node as its successor
+        n1.handle_request(result)
+        self.assertEqual(n1.successor, d0)
+
+
+    def test_not_responsible_for_key(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        d1 = node_ranked(1);    n1 = node.NodeCore(d1)
+
+        # Join nodes 0 and 1
+        result = n0.handle_request(node.Join(d1))
+        n1.handle_request(result)
+
+        k1 = key_ranked(1)
+        self.assertEqual(n0.responsible_for_key(k1), False)
+        self.assertEqual(n1.responsible_for_key(k1), True)
+
+    def test_join_double_direct(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        d1 = node_ranked(1);    n1 = node.NodeCore(d1)
+        d2 = node_ranked(2);    n2 = node.NodeCore(d2)
+
+        # Join nodes 0 and 1
+        result = n0.handle_request(node.Join(d1))
+        n1.handle_request(result)
+
+        # Add node 2 at 1
+        result = n1.handle_request(node.Join(d2))
+
+        # Node 1 should accept the request
+        self.assertEqual(isinstance(result, node.JoinAccepted), True)
+        self.assertEqual(result.successor, d0)
+
+        # Node 1 should accept node 2 as its new successor
+        self.assertEqual(n1.successor, d2)
+
+        # New node (2) should take the given node as its successor
+        n2.handle_request(result)
+        self.assertEqual(n2.successor, d0)
+
+
+    def test_join_double_indirect(self):
+        d0 = node_ranked(0);    n0 = node.NodeCore(d0)
+        d1 = node_ranked(1);    n1 = node.NodeCore(d1)
+        d2 = node_ranked(2);    n2 = node.NodeCore(d2)
+
+        # Join nodes 0 and 1
+        result = n0.handle_request(node.Join(d1))
+        n1.handle_request(result)
+
+        # Add node 2 at 0
+        fwd = n0.handle_request(node.Join(d2))
+
+        # Node 0 should forward the request
+        self.assertEqual(isinstance(fwd, node.ForwardRequest), True)
+        self.assertEqual(fwd.destination, d1)
+
+        # Deliver forward request
+        result = n1.handle_request(fwd.request)
+
+        # Node 1 should accept the request
+        self.assertEqual(isinstance(result, node.JoinAccepted), True)
+        self.assertEqual(result.successor, d0)
+
+        # Node 1 should accept node 2 as its new successor
+        self.assertEqual(n1.successor, d2)
+
+        # New node (2) should take the given node as its successor
+        n2.handle_request(result)
+        self.assertEqual(n2.successor, d0)
+
+
+if __name__ == '__main__':
+    unittest.main()
