84a8928a4df5e34e0b6e7ea3df987d53a7e56b00
Mike Murphy <codermurph@sleepymurph.com>
2015-10-30 17:20:19 +0100

    Testing: incorporate test script from Magnus

--- assignment2/src/local_test.sh
+++ assignment2/src/local_test.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+# First round. Join up some nodes with verbose output to watch what happens.
 python -u ./node2_http.py -v localhost:8000 &
 sleep 1
 python -u ./node2_http.py -v localhost:8001 --join localhost:8000 &
@@ -15,5 +16,21 @@ curl -vX GET http://localhost:8002/getNodes
 curl -vX GET http://localhost:8000/getCurrentLeader
 curl -vX GET http://localhost:8001/getCurrentLeader
 curl -vX GET http://localhost:8002/getCurrentLeader
-sleep 5
+sleep 1
+kill $(jobs -p)
+
+# New round. Join up nodes with quite output and use tool to examine network.
+sleep 1
+python -u ./node2_http.py localhost:8000 2>/dev/null &
+sleep 1
+python -u ./node2_http.py localhost:8001 --join localhost:8000 2>/dev/null &
+sleep 1
+python -u ./node2_http.py localhost:8002 --join localhost:8000 2>/dev/null &
+sleep 1
+python -u ./node2_http.py localhost:8003 --join localhost:8000 2>/dev/null &
+sleep 1
+python -u ./node2_http.py localhost:8004 --join localhost:8000 2>/dev/null &
+sleep 1
+python -u leader_benchmark.py --ip localhost --port 8000
+sleep 1
 kill $(jobs -p)
