4bc0608ca93ef4cb0727acf398152a17ef406d2c
Mike Murphy <codermurph@sleepymurph.com>
2015-11-04 03:18:08 +0100

    Logging: replace custom 'verbosep' with logging

--- assignment2/src/node2_http.py
+++ assignment2/src/node2_http.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
 
 import BaseHTTPServer
+import Queue
 import argparse
 import collections
 import httplib
-import Queue
+import logging
 import signal
 import threading
 
 import node_core2 as ncore
 
-# Verbose output trace
+# Logging Setup
 
-verbose = False
+loghandler = logging.StreamHandler()
+loghandler.setFormatter(logging.Formatter(
+    '%(msecs)03.4f %(name)-12s %(levelname)-8s -- %(core)s - %(message)s'))
 
-def verbosep(msg):
-    """ Print string if verbose output is on """
-    if verbose:
-        if server_node_core:
-            print("%s: %s" % (server_node_core.descriptor.host_port, msg))
+logger = logging.getLogger("node_http")
+logger.setLevel(logging.INFO)
+logger.addHandler(loghandler)
+
+class CoreFilter(logging.Filter):
+    def filter(self, record):
+        if (server_node_core):
+            record.core = server_node_core.descriptor
         else:
-            print(strs)
+            record.core = "no core"
+        return True
+
+logger.addFilter(CoreFilter())
 
 
 # HTTP Abstractions
@@ -201,7 +210,7 @@ def build_response(dr):
 
 def send_message(msg):
     """ Send a message to another node """
-    verbosep("Sending message: %s" % (msg,))
+    logger.debug("Sending message: %s" % (msg,))
     hr = build_request(msg)
     send_request(hr)
 
@@ -211,7 +220,7 @@ def send_request(hr):
 
     conn = httplib.HTTPConnection(hr.host, hr.port)
     conn.request(hr.method, hr.path, hr.body)
-    verbosep("Sent request: %s:%d %s %s '%s'" %
+    logger.debug("Sent request: %s:%d %s %s '%s'" %
             (hr.host, hr.port, hr.method, hr.path, hr.body))
 
     # Must read response even if we don't do anything with it.
@@ -227,7 +236,7 @@ def send_request(hr):
     if response.status!=200:
         raise RuntimeException("Got bad response: %s" % response.status)
 
-    verbosep("Got OK response: %s %s" % (hr.method, hr.path))
+    logger.debug("Got OK response: %s %s" % (hr.method, hr.path))
 
 
 # Actually Run as a Server
@@ -248,7 +257,7 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.handle_request("PUT")
 
     def handle_request(self, method):
-        verbosep("Receiving request: %s %s" % (method, self.path))
+        logger.debug("Receiving request: %s %s" % (method, self.path))
         content_length = int(self.headers.getheader('content-length', 0))
         body = self.rfile.read(content_length)
 
@@ -257,7 +266,7 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 method = method,
                 path = self.path,
                 body = body))
-        verbosep("Parsed message: %s" % (msg,))
+        logger.debug("Parsed message: %s" % (msg,))
 
         action = server_node_core.handle_message(msg)
 
@@ -267,7 +276,7 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def send_response_for_action(self, action):
         hr = build_response(action)
-        verbosep("Responding with %s" % (action,))
+        logger.debug("Responding with %s" % (action,))
         self.send_response(hr.status)
         self.end_headers()
         self.wfile.write(hr.body)
@@ -314,7 +323,7 @@ class MessageQueue:
 
     def start(self):
 
-        verbosep("Starting message queue")
+        logger.debug("Starting message queue")
         def sender():
             while True:
                 msg = self.outbox.get()
@@ -332,9 +341,9 @@ class MessageQueue:
 
     def stop(self):
         """ Waits until current messages are sent, then quits """
-        verbosep("Stopping message queue after empty...")
+        logger.debug("Stopping message queue after empty...")
         self.outbox.join()
-        verbosep("Message queue empty, shutting down.")
+        logger.debug("Message queue empty, shutting down.")
 
 
 
@@ -349,13 +358,16 @@ if __name__ == '__main__':
             help="another server to join")
     args = parser.parse_args()
 
-    verbose = args.verbose
+    if args.verbose:
+        ncore.logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+
     descriptor = ncore.NodeDescriptor(host_port=args.host_port)
     join_descriptor = ncore.NodeDescriptor(host_port=args.join) if args.join else None
     server_node_core = ncore.NodeCore(descriptor)
 
     # Start the webserver which handles incomming requests
-    verbosep("Starting HTTP server: %s, %s" % (descriptor, descriptor.rank))
+    logger.debug("Starting HTTP server: %s, %s" % (descriptor, descriptor.rank))
     httpd = NodeServer((descriptor.host, descriptor.port), HttpRequestHandler)
     server_thread = threading.Thread(target = httpd.serve)
     server_thread.daemon = True
@@ -366,7 +378,7 @@ if __name__ == '__main__':
     message_queue.start()
 
     def handler(signum, frame):
-        verbosep("Caught signal %d, stopping http server..." % signum)
+        logger.debug("Caught signal %d, stopping http server..." % signum)
         httpd.stop()
         message_queue.stop()
     signal.signal(signal.SIGINT, handler)
